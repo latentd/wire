b36fe5c24b3796e5abbffef2bed6b20f055900c0
latentd <latentd0@gmail.com>
2021-03-02 01:30:51 +0900

    add path regex

--- main.go
+++ main.go
@@ -26,7 +26,17 @@ func t() http.Handler {
 }
 
 func t2(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("test2"))
+	ctx := r.Context()
+
+	v := ctx.Value("id")
+	s, ok := v.(string)
+	fmt.Println(ok)
+
+	v2 := ctx.Value("nid")
+	s2, ok := v2.(string)
+	fmt.Println(ok)
+
+	w.Write([]byte(s + s2))
 }
 
 func main() {
@@ -36,9 +46,10 @@ func main() {
 	w.Chain(l1, l2)
 	w.Get("/test", t())
 
-	w2 := w.SubRouter("/api")
-	w2.GetF("/a", t2)
-	w2.GetF("/a/([1-9]+)/b", t2)
+	w2 := w.SubRouter("/api/(id:[1-9]+)")
+
+	w3 := w2.SubRouter("/action")
+	w3.GetF("/(nid:[1-9]+)", t2)
 	//r2.GetF("/a/(id:[1-9]+)/b/(nid:[1-9]+)/c", t2)
 
 	http.ListenAndServe(":8080", w)
--- wire.go
+++ wire.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"net/http"
 	"reflect"
 	"regexp"
@@ -15,7 +15,13 @@ type Wire struct {
 
 type route struct {
 	pattern *regexp.Regexp
+	vars    []*pathVar
 	execs   []*routeExec
+}
+
+type pathVar struct {
+	name    string
+	pattern *regexp.Regexp
 }
 
 type routeExec struct {
@@ -33,16 +39,42 @@ func (wi *Wire) Chain(middlewares ...func(http.Handler) http.Handler) {
 	}
 }
 
+var nRgxp = regexp.MustCompile(`\((.*?):`)
+
+func (wi *Wire) normalizePath(path string) string {
+	path = nRgxp.ReplaceAllString(path, "(")
+	// if path[len(path)-1:] != "/" {
+	// 	path += "$"
+	// }
+	return path
+}
+
+var fRgxp = regexp.MustCompile(`\(.*?\)`)
+
+func (wi *Wire) findPathVars(path string) []*pathVar {
+	nPath := wi.normalizePath(path)
+
+	nms := nRgxp.FindAllStringSubmatch(path, -1)
+	fms := fRgxp.FindAllStringSubmatch(nPath, -1)
+
+	var vars []*pathVar
+	for i, fm := range fms {
+		vars = append(vars, &pathVar{nms[i][1], regexp.MustCompile(fm[0])})
+	}
+
+	return vars
+}
+
 func (wi *Wire) registerHandler(path string, method string, h http.Handler) {
 
 	re := &routeExec{method: method, handler: h}
 
+	nPath := wi.normalizePath(path)
+	pattern := regexp.MustCompile("^" + wi.prefix + nPath)
+
+	pathVars := wi.findPathVars(path)
+
 	var pathExists bool
-	str := "^%s$"
-	if path[len(path)-1:] == "/" {
-		str = "^%s"
-	}
-	pattern := regexp.MustCompile(fmt.Sprintf(str, wi.prefix+path))
 	for _, r := range wi.routes {
 		if reflect.DeepEqual(r.pattern, pattern) {
 			r.execs = append(r.execs, re)
@@ -50,13 +82,35 @@ func (wi *Wire) registerHandler(path string, method string, h http.Handler) {
 		}
 	}
 	if !pathExists {
-		wi.routes = append(wi.routes, &route{pattern, []*routeExec{re}})
+		wi.routes = append(
+			wi.routes,
+			&route{
+				pattern,
+				pathVars,
+				[]*routeExec{re},
+			},
+		)
 	}
 }
 
 func handlerWithStatus(status int) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(status)
+	})
+}
+
+var aNorm = regexp.MustCompile(`\((.*?)\)`)
+
+func wrapHandlerWithVars(h http.Handler, vars []*pathVar, path string) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		for _, v := range vars {
+			loc := v.pattern.FindStringIndex(path)
+			ctx = context.WithValue(ctx, v.name, path[loc[0]:loc[1]])
+			path = path[loc[1]:]
+		}
+		r = r.WithContext(ctx)
+		h.ServeHTTP(w, r)
 	})
 }
 
@@ -67,10 +121,10 @@ func (wi *Wire) lookupHandler(req *http.Request) http.Handler {
 	for _, r := range wi.routes {
 		if r.pattern.MatchString(req.URL.Path) {
 			pathFound = true
-			for _, r := range r.execs {
-				if r.method == req.Method || r.method == "ALL" {
+			for _, e := range r.execs {
+				if e.method == req.Method || e.method == "ALL" {
 					methodAllowed = true
-					h = r.handler
+					h = wrapHandlerWithVars(e.handler, r.vars, req.URL.Path)
 					break
 				}
 			}
@@ -96,7 +150,7 @@ func (wi *Wire) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 func (wi *Wire) SubRouter(path string) *Wire {
 	nwi := NewWire()
-	nwi.prefix = wi.prefix + path
+	nwi.prefix = wi.prefix + wi.normalizePath(path)
 	wi.registerHandler(path+"/", "ALL", nwi)
 	return nwi
 }
