26d39221c68ce9a5c7482f4b19b62a08d39eae10
latentd <latentd0@gmail.com>
2021-03-03 00:56:37 +0900

    update test

--- wire_test.go
+++ wire_test.go
@@ -7,7 +7,33 @@ import (
 	"testing"
 )
 
-func TestRouter(t *testing.T) {
+func TestRootRouting(t *testing.T) {
+
+	tts := []struct {
+		name   string
+		path   string
+		method string
+		want   int
+	}{
+		{
+			name:   "registered handler returns 200",
+			path:   "/",
+			method: http.MethodGet,
+			want:   http.StatusOK,
+		},
+		{
+			name:   "unregistered method returns 405",
+			path:   "/",
+			method: http.MethodPost,
+			want:   http.StatusMethodNotAllowed,
+		},
+        {
+            name: "unregistered path returns 404",
+            path: "/test",
+            method: http.MethodGet,
+            want: http.StatusNotFound,
+        },
+	}
 
 	r := NewRouter()
 
@@ -18,24 +44,32 @@ func TestRouter(t *testing.T) {
 	srv := httptest.NewServer(r)
 	defer srv.Close()
 
-	cli := &http.Client{}
-	req, err := http.NewRequestWithContext(
-		context.TODO(),
-		http.MethodGet,
-		srv.URL,
-		nil,
-	)
-	if err != nil {
-		t.Fatal(err)
-	}
+	for _, tt := range tts {
 
-	res, err := cli.Do(req)
-	if err != nil {
-		t.Fatal(err)
-	}
+		t.Run(tt.name, func(t *testing.T) {
 
-	if res.StatusCode != http.StatusOK {
-		t.Errorf("want %d, got %d", http.StatusOK, res.StatusCode)
+			cli := &http.Client{}
+
+			req, err := http.NewRequestWithContext(
+				context.TODO(),
+				tt.method,
+				srv.URL+tt.path,
+				nil,
+			)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			res, err := cli.Do(req)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if res.StatusCode != tt.want {
+				t.Errorf("want %d, got %d", tt.want, res.StatusCode)
+			}
+		})
+
 	}
 
 }
